5463e3926f494c5d4040579779da0d0215b38410
kkkkkkkkkwon <ch80551@gmail.com>
2025-10-07 15:28:15 +0900

    chore: initial commit

---
+++ src/main/java/gyeonggi/gyeonggifesta/GyeonggifestaApplication.java
@@ -0,0 +1,13 @@
+package gyeonggi.gyeonggifesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeonggifestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyeonggifestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gyeonggi/gyeonggifesta/GyeonggifestaApplicationTests.java
@@ -0,0 +1,13 @@
+package gyeonggi.gyeonggifesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyeonggifestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
