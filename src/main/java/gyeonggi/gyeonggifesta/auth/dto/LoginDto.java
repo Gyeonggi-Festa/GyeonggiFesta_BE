2c28367e37a06e33635a332fb687f2c1665d67e6
kkkkkkkkkwon <ch80551@gmail.com>
2025-10-07 16:19:01 +0900

    WIP: save current work

---
+++ src/main/java/gyeonggi/gyeonggifesta/auth/dto/LoginDto.java
@@ -0,0 +1,17 @@
+package gyeonggi.gyeonggifesta.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginDto {
+
+	private String email;
+	private String verifyId;
+	private String role;
+}
