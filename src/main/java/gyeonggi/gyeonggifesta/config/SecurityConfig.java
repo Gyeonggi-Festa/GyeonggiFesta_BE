1c00d88d7a59b57ce1f2b6ec42080864697c3e2e
kkkkkkkkkwon <ch80551@gmail.com>
2025-11-11 23:11:42 +0900

    fix(security): 중복 문제 해결

--- src/main/java/gyeonggi/gyeonggifesta/config/SecurityConfig.java
+++ src/main/java/gyeonggi/gyeonggifesta/config/SecurityConfig.java
@@ -116,95 +116,9 @@ public class SecurityConfig {
 		return source;
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-    private static final List<String> CORS_WHITELIST = List.of(
-            "https://gyeonggi-festa.vercel.app",
-			"http://43.202.50.44:8080",
-            "http://localhost:5173",
-            "http://127.0.0.1:5500"
-    );
-
-    private static final List<String> WHITELIST = List.of(
-            "/login",
-            "/api/register",
-            "/api/login",
-            "/api/token/**",
-            "/api/dev/**",  // 개발용 임시 로그인
-            "/ws-stomp/**",
-            "/batch/event-sync/run" // 수동 배치 -> 개발때문에 잠시 넣은거
-    );
-
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final OAuth2FailureHandler oAuth2FailureHandler;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    @Profile("local")
-    public SecurityFilterChain localFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .oauth2Login(AbstractHttpConfigurer::disable) // 완전 비활성화
-
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(WHITELIST.toArray(new String[0])).permitAll()
-                        .anyRequest().authenticated())
-
-                .addFilterBefore(new JwtFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class)
-
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-
-        return http.build();
-    }
-
-    @Bean
-    @Profile("!local") // 운영/개발 서버
-    public SecurityFilterChain prodFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-
-                .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
-                        .successHandler(oAuth2SuccessHandler)
-                        .failureHandler(oAuth2FailureHandler))
-
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(WHITELIST.toArray(new String[0])).permitAll()
-                        .anyRequest().authenticated())
-
-                .addFilterBefore(new JwtFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-
-        return http.build();
-    }
-
-    private CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(CORS_WHITELIST);
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
