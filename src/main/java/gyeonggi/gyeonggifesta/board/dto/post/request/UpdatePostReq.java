031faace32eec8f7f7d210b4dc149467fd244f76
kkkkkkkkkwon <ch80551@gmail.com>
2025-11-14 02:29:41 +0900

    Add jackson-compatible constructors to post request DTOs

--- src/main/java/gyeonggi/gyeonggifesta/board/dto/post/request/UpdatePostReq.java
+++ src/main/java/gyeonggi/gyeonggifesta/board/dto/post/request/UpdatePostReq.java
@@ -3,14 +3,18 @@ package gyeonggi.gyeonggifesta.board.dto.post.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import gyeonggi.gyeonggifesta.board.enums.AgeRange;
 import gyeonggi.gyeonggifesta.board.enums.GenderPreference;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdatePostReq {
         private String title;
         private String content;
