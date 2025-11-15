18b3d3f879bd8dbe9bf9db9e2960c28952639078
popeye <popeye0618@gmail.com>
2025-11-16 03:12:48 +0900

    refactor($event): 좋아요 여부 수정

--- src/main/java/gyeonggi/gyeonggifesta/event/service/event/EventServiceImpl.java
+++ src/main/java/gyeonggi/gyeonggifesta/event/service/event/EventServiceImpl.java
@@ -5,12 +5,14 @@ import gyeonggi.gyeonggifesta.event.dto.event.response.EventDetailRes;
 import gyeonggi.gyeonggifesta.event.dto.event.response.EventRes;
 import gyeonggi.gyeonggifesta.event.entity.Event;
 import gyeonggi.gyeonggifesta.event.entity.EventFavorite;
+import gyeonggi.gyeonggifesta.event.entity.EventLike;
 import gyeonggi.gyeonggifesta.event.entity.EventSearchHistory;
 import gyeonggi.gyeonggifesta.event.exception.EventErrorCode;
 import gyeonggi.gyeonggifesta.event.repository.EventRepository;
 import gyeonggi.gyeonggifesta.event.repository.EventSearchHistoryRepository;
 import gyeonggi.gyeonggifesta.event.repository.EventSpecifications;
 import gyeonggi.gyeonggifesta.event.service.favorite.EventFavoriteService;
+import gyeonggi.gyeonggifesta.event.service.like.EventLikeService;
 import gyeonggi.gyeonggifesta.exception.BusinessException;
 import gyeonggi.gyeonggifesta.member.entity.Member;
 import gyeonggi.gyeonggifesta.util.security.SecurityUtil;
@@ -32,6 +34,7 @@ public class EventServiceImpl implements EventService {
 	private final EventRepository eventRepository;
 	private final EventSearchHistoryRepository eventSearchHistoryRepository;
 	private final EventFavoriteService eventFavoriteService;
+	private final EventLikeService eventLikeService;
 
 	@Override
 	@Transactional
@@ -95,23 +98,23 @@ public class EventServiceImpl implements EventService {
 	 */
 	private EventRes convertToEventRes(Event event) {
 		return EventRes.builder()
-				.eventId(event.getId())
-				.title(event.getTitle())
-				.category(event.getCodename())
-				.isFree(event.getIsFree())
-				.status(event.getStatus().toString())
-				.startDate(event.getStartDate())
-				.endDate(event.getEndDate())
-				.mainImg(event.getMainImg())
-				.rating(event.getRating())
-				.likes(event.getLikes())
-				.favorites(event.getFavorites())
-				.comments(event.getComments())
-				.ratingCount(event.getEventReviews().size())
-				.latitude(event.getLatitude())
-				.longitude(event.getLongitude())
-				.roadAddress(event.getRoadAddress())
-				.build();
+			.eventId(event.getId())
+			.title(event.getTitle())
+			.category(event.getCodename())
+			.isFree(event.getIsFree())
+			.status(event.getStatus().toString())
+			.startDate(event.getStartDate())
+			.endDate(event.getEndDate())
+			.mainImg(event.getMainImg())
+			.rating(event.getRating())
+			.likes(event.getLikes())
+			.favorites(event.getFavorites())
+			.comments(event.getComments())
+			.ratingCount(event.getEventReviews().size())
+			.latitude(event.getLatitude())
+			.longitude(event.getLongitude())
+			.roadAddress(event.getRoadAddress())
+			.build();
 	}
 
 	@Override
@@ -120,34 +123,37 @@ public class EventServiceImpl implements EventService {
 		Member currentMember = securityUtil.getCurrentMember();
 
 		Event event = eventRepository.findById(eventId)
-				.orElseThrow(() -> new BusinessException(EventErrorCode.NOT_EXIST_EVENT));
+			.orElseThrow(() -> new BusinessException(EventErrorCode.NOT_EXIST_EVENT));
 
 		EventFavorite favorite = eventFavoriteService.getEventFavoriteByEvent(event);
+		EventLike like = eventLikeService.getEventLikeByEvent(event);
 
 		boolean isFavorite = currentMember.getEventFavorites().contains(favorite);
+		boolean isLiked = currentMember.getEventLikes().contains(like);
 
 		return EventDetailRes.builder()
-				.eventId(event.getId())
-				.status(event.getStatus().name())
-				.category(event.getCodename())
-				.title(event.getTitle())
-				.orgName(event.getOrgName())
-				.useFee(event.getUseFee())
-				.timeInfo(event.getTimeInfo())
-				.orgLink(event.getOrgLink())
-				.mainImg(event.getMainImg())
-				.startDate(event.getStartDate())
-				.endDate(event.getEndDate())
-				.isFree(event.getIsFree())
-				.likes(event.getLikes())
-				.favorites(event.getFavorites())
-				.comments(event.getComments())
-				.rating(event.getRating())
-				.isFavorite(isFavorite)
-				.latitude(event.getLatitude())
-				.longitude(event.getLongitude())
-				.roadAddress(event.getRoadAddress())
-				.build();
+			.eventId(event.getId())
+			.status(event.getStatus().name())
+			.category(event.getCodename())
+			.title(event.getTitle())
+			.orgName(event.getOrgName())
+			.useFee(event.getUseFee())
+			.timeInfo(event.getTimeInfo())
+			.orgLink(event.getOrgLink())
+			.mainImg(event.getMainImg())
+			.startDate(event.getStartDate())
+			.endDate(event.getEndDate())
+			.isFree(event.getIsFree())
+			.likes(event.getLikes())
+			.favorites(event.getFavorites())
+			.comments(event.getComments())
+			.rating(event.getRating())
+			.isFavorite(isFavorite)
+			.isLiked(isLiked)
+			.latitude(event.getLatitude())
+			.longitude(event.getLongitude())
+			.roadAddress(event.getRoadAddress())
+			.build();
 	}
 
 }
